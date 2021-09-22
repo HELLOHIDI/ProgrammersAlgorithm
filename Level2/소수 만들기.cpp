b71373bca3ac63d84eec58bc9232b3793cf0db9f
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-09-22 22:47:42 +0900

    Create 소수 만들기.cpp

---
+++ Level2/소수 만들기.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool check(int temp){
+    for(int i = 2; i < temp/2; i++){
+        if(temp%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = i+1; j < nums.size(); j++){
+            for(int k = j+1; k < nums.size(); k++){
+                int temp = nums[i] + nums[j] + nums[k];
+                bool checking = check(temp);
+                if(checking){
+                    answer++;
+                }
+            }
+        }
+    }    
+
+    return answer;
+}
